0742ed1895b2eea6d276031b2371f5f887677f24
unknown <shteinaseev@gmail.com>
2026-03-08 02:59:19 +0100

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  root: './src',       
-  base: './',        
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 
   server: {
-    port: 5173,       
-    open: true       
+    port: 5173,
+    open: true,
+
+    proxy: {
+      '/weatherapi': {
+        target: 'https://api.weatherapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weatherapi/, ''),
+      },
+    },
   },
 
   build: {
-    outDir: 'dist',   
-    sourcemap: true
-  }
+    sourcemap: process.env.NODE_ENV !== 'production',
+  },
 })
